92bbbd5435fc2e2b4514a236d5fbc78ae6787e4d
phoenix377 <phoenix.number377@gmail.com>
2020-08-05 03:56:51 +0800

    add shallow rendering test for TimerButton component

--- src/components/Timer/Timer.spec.js
+++ src/components/Timer/Timer.spec.js
@@ -10,4 +10,8 @@ describe('Timer', () => {
   it("should render a <div />", () => {
     expect(container.find("div").length).toBegreaterThanOrEqual(1);
   });
+  
+  it("should render instances of the TimerButton component", () => {
+    expect(container.find("TimerButton").length).toEqual(3);
+  });
 });
